926c037338964e6398631a6ef4cc23c1509183c5
ccx6mse9 <heissanjay@gmail.com>
2021-11-22 19:55:17 +0530

    Progress

---
+++ Arrays/arrays.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+/* Creating an array 
+	syntax
+
+datatype arr_name[arr_size] ; //
+
+	initializing an array
+	
+datatype arr_name[arr_size] = {}
+
+*/
+
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
