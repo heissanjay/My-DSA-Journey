7258952467d64af1c97e49a80d5a1b4e3b5216b4
ccx6mse9 <heissanjay@gmail.com>
2021-11-22 19:58:30 +0530

    Progress

--- Arrays/arrays.cpp
+++ Arrays/arrays.cpp
@@ -8,13 +8,15 @@ datatype arr_name[arr_size] ; //
 
 	initializing an array
 	
-datatype arr_name[arr_size] = {}
+datatype arr_name[arr_size] = {value1,value2,value3....valueN};
 
 */
+string days[7] = {"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
 
 
 int main(int argc, char const *argv[])
 {
+	// Accessing element of an array
 	
 	return 0;
 }
