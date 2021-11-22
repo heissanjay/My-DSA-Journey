ac477b8373517cbb02e3c9397ee6233dc4626d80
ccx6mse9 <heissanjay@gmail.com>
2021-11-22 20:07:39 +0530

    Progress

--- Arrays/arrays.cpp
+++ Arrays/arrays.cpp
@@ -19,7 +19,15 @@ int main(int argc, char const *argv[])
 	// Accessing element of an array
 	// Array index is start from 0
 
-	cout<<"First day of a week is "<<days[0]<<endl;
+	// cout<<"First day of a week is "<<days[0]<<endl;
+
+	/* we can able to loop through th array */
+	cout<<"The days in a week are "<<endl;
+	for (int i = 0; i < (sizeof(days)/sizeof(days[0])); ++i)
+	{
+		
+		cout<<i+1<<". "<<days[i]<<" "<<"\n";
+	}
 	
 	return 0;
 }
