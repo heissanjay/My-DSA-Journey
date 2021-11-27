e4666cd4631ced9bec291b3364c7182df6d809df
ccx6mse9 <heissanjay@gmail.com>
2021-11-27 19:19:08 +0530

    Progress

--- Stacks/Stacks.cpp
+++ Stacks/Stacks.cpp
@@ -11,14 +11,25 @@ class Stack{
 public:
 	int arr[MAX];
 	Stack() {top = -1;}
-	bool push();
+	bool push(int val);
 	int pop();
 	int peek();
 	bool isEmpty();
 };
+// function to push the element to the stack
+bool Stack::push(int val){
+	if(top >= (MAX - 1)){
+		cout<<"Stack OverFlow"<<endl;
+		return false;
+	}else{
+		arr[top++]=val;
+		return true;
+	}
+}
 
 int main(int argc, char const *argv[])
 {
-	
+	class Stack stk;
+	stk.push(3);
 	return 0;
 }
