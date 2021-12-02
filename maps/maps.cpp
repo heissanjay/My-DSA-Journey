c02f5a6f76e4ab54026494b98e7644d3c86681f1
ccx6mse9 <heissanjay@gmail.com>
2021-12-02 18:52:25 +0530

    Progressing

--- maps/maps.cpp
+++ maps/maps.cpp
@@ -5,9 +5,15 @@
 using namespace std;
 
 
-
-
-
-
 /* Defining a Map */
-map<int, string> numInWords;
+map<int,string> numInWords;
+map<int,string>::iterator iter;
+
+int main(int argc, char const *argv[])
+{
+	/*assing values to the map */
+	numInWords.insert(pair<int,string>(1,"one"));
+	cout<<(numInWords.begin())->first<<"\t";
+	cout<<(numInWords.begin())->second<<endl;
+	return 0;
+}
