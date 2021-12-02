c269617713ba575a7ab51036b153d428433b8b34
ccx6mse9 <heissanjay@gmail.com>
2021-12-02 19:02:51 +0530

    Progressing

--- maps/maps.cpp
+++ maps/maps.cpp
@@ -12,14 +12,22 @@ map<int,string>::iterator iter;
 int main(int argc, char const *argv[])
 {
 	/*assing values to the map */
-	numInWords.insert(pair<int,string>(1,"one"));
-	numInWords.insert(pair<int,string>(2,"two"));
-	numInWords.insert(pair<int,string>(3,"three"));
-	numInWords.insert(pair<int,string>(4,"four"));
-	numInWords.insert(pair<int,string>(5,"five"));
-	numInWords.insert(pair<int,string>(6,"six"));
+	// numInWords.insert(pair<int,string>(1,"one"));
+	// numInWords.insert(pair<int,string>(2,"two"));
+	// numInWords.insert(pair<int,string>(3,"three"));
+	// numInWords.insert(pair<int,string>(4,"four"));
+	// numInWords.insert(pair<int,string>(5,"five"));
+	// numInWords.insert(pair<int,string>(6,"six"));
 
-	cout<<(numInWords.find(6))->first<<"\t";
-	cout<<(numInWords.find(6))->second<<endl;
+	/* alternate way of inserting */
+	numInWords.insert({1,"one"});
+	numInWords.insert({2,"two"});
+	numInWords.insert({3,"three"});
+	numInWords.insert({4,"four"});
+	numInWords.insert({5,"five"});
+	numInWords.insert({6,"six"});
+
+	cout<<(numInWords.find(4))->first<<"\t";
+	cout<<(numInWords.find(4))->second<<endl;
 	return 0;
 }
