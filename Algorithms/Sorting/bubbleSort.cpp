e00f7b050f50885659418338403a5899b4902749
ccx6mse9 <heissanjay@gmail.com>
2021-12-08 15:36:45 +0530

    Progressing

--- Algorithms/Sorting/bubbleSort.cpp
+++ Algorithms/Sorting/bubbleSort.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	int arr[10] = {12,3,56,78,6,45,23,89,1,7};
-	int arrlen = sizeof(arr)/sizeof(arr[0])
+	int arrlen = sizeof(arr)/sizeof(arr[0]);
 	for (int i = 0; i < arrlen; ++i)
 	{
 		cout<<arr[i]<<" ";
