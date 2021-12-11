84486a7ae3b08b3dc0d139190cb3c41ebd689bce
ccx6mse9 <heissanjay@gmail.com>
2021-12-11 21:39:28 +0530

    added Problem set

--- Problem-set/Linked-List/llProblems.cpp
+++ Problem-set/Linked-List/llProblems.cpp
@@ -37,3 +37,56 @@ public:
 };
 
 // https://leetcode.com/problems/linked-list-cycle-ii/
+
+class Solution {
+public:
+    int findLength(ListNode *head){
+        ListNode* fast  = head;
+        ListNode* slow = head;
+        
+        while(fast != nullptr && fast->next != nullptr){
+            fast = fast->next->next;
+            slow = slow->next;
+           
+            if(fast == slow){
+                ListNode* temp = slow;
+                int length = 0;
+                do {
+                    temp = temp->next;
+                    length++;
+                } while (temp != slow);
+                return length;
+            }  
+        }
+        return 0;        
+    }
+    ListNode *detectCycle(ListNode *head) {
+        int length = 0;
+        ListNode* fast  = head;
+        ListNode* slow = head;
+        while(fast != nullptr && fast->next != nullptr){
+            fast = fast->next->next;
+            slow = slow->next;
+            if(fast == slow){
+                length = findLength(slow);break;
+            }
+        }
+        if(length == 0){
+            return NULL;
+        }
+        ListNode* fst = head;
+        ListNode* slw = head;
+        
+        while(length>0){
+            slw = slw->next;
+            length--;
+        }
+       
+        while(fst!=slw){
+            fst = fst->next;
+            slw = slw->next;
+        }
+        
+        return slw;
+    }
+};
