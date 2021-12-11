f37c20d80a87a20eecaf72851ce16fe3447cb303
ccx6mse9 <heissanjay@gmail.com>
2021-12-11 21:37:40 +0530

    added Problem set

--- Problem-set/Linked-List/llProblems.cpp
+++ Problem-set/Linked-List/llProblems.cpp
@@ -19,3 +19,19 @@ public:
 
 // https://leetcode.com/problems/linked-list-cycle/
 
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* fast  = head;
+        ListNode* slow = head;
+        
+        while(fast != nullptr && fast->next != nullptr){
+            fast = fast->next->next;
+            slow = slow->next;
+            if(fast == slow){
+                return true;
+            }
+        }
+        return false;
+    }
+};
