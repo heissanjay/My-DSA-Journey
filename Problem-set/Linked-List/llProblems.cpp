e3d1dc4aeba851b6090412f9ff45f98fa8a4297d
ccx6mse9 <heissanjay@gmail.com>
2021-12-13 21:07:42 +0530

    added Problem set

--- Problem-set/Linked-List/llProblems.cpp
+++ Problem-set/Linked-List/llProblems.cpp
@@ -120,3 +120,16 @@ public:
 };
 
 https://leetcode.com/problems/middle-of-the-linked-list/
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        
+        while(fast!=nullptr && fast->next != nullptr){
+            fast = fast->next->next;
+            slow= slow->next;
+        }
+        return slow;
+    }
+};
