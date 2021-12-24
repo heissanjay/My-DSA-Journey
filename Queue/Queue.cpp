7a7baa710d5eb6d4c22e442dd982d6e05f967126
ccx6mse9 <heissanjay@gmail.com>
2021-12-24 19:56:12 +0530

    doing with queue

--- Queue/Queue.cpp
+++ Queue/Queue.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 using namespace std;
+#define SIZE 100
 
-int main(int argc,char *argv[]){
+class Queue
+{
+public:
+	int arr[SIZE];
+	int front = -1, rear = -1;
 
+	bool isEmpty()
+	{
+		if (front == rear == -1)
+			return true;
+		else
+			return false;
+	}
+};
+
+int main(int argc, char *argv[])
+{
+	Queue Que1;
 	return 0;
 }
