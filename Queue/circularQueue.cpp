b364f7d9e924fe53efd68733bcb925ee86c48c74
ccx6mse9 <heissanjay@gmail.com>
2022-01-21 13:58:53 +0530

    Progress Queue

--- Queue/circularQueue.cpp
+++ Queue/circularQueue.cpp
@@ -10,7 +10,19 @@ public:
     void enqueue(int val);
     void dequeue();
 };
+void CircularQueue::enqueue(int val)
+{
+    if (((front == 0) && (rear = MAX - 1)) || (rear + 1 == front))
+        cout << "queue is full";
+    else if (rear == -1)
+        front = rear = 0;
+    else if (rear = MAX - 1)
+        rear = 0;
+    else
+        rear++;
 
+    arr[rear] = val;
+}
 int main(int argc, char const *argv[])
 {
 
