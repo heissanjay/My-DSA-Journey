9bb90f19c878f08b2df51cba630608eacb7a2a2e
ccx6mse9 <heissanjay@gmail.com>
2022-01-25 15:03:56 +0530

    binary trees

---
+++ Trees/binaryTree.cpp
@@ -0,0 +1,14 @@
+// implementation of binary tree
+
+// including required libraries
+
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+
+// structure of node
+struct Node
+{
+    int item;
+    struct Node *left, *right;
+};
