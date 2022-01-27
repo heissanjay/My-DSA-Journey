6ab8ed0be6cca25781e8a76f957e5f950faf76a1
ccx6mse9 <heissanjay@gmail.com>
2022-01-27 18:31:17 +0530

    linear search

--- Algorithms/Searching/linearSearch.cpp
+++ Algorithms/Searching/linearSearch.cpp
@@ -4,9 +4,20 @@
 using namespace std;
 
 #define SIZE 10
-int linearSearch(int target);
+int linearSearch(int target); // function prototype
+
+int arr[SIZE] = {34, 26, 12, 89, 45, 69, 90, 345, 73, 56};
 int main(int argc, char const *argv[])
 {
 
     return 0;
+}
+int linearSearch(int target)
+{
+    for (int i = 0; i < SIZE; ++i)
+    {
+        if (arr[i] == target)
+            return i;
+    }
+    return -1;
 }
