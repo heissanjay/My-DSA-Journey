cff0d07323d68dca2ff12b6654ca44a1c3eee7c7
ccx6mse9 <heissanjay@gmail.com>
2022-01-27 18:20:18 +0530

    linear search

---
+++ Algorithms/Searching/linearSearch.cpp
@@ -0,0 +1,12 @@
+// implementation of linear search
+
+#include <iostream>
+using namespace std;
+
+#define SIZE 10
+int linearSearch(int target);
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
