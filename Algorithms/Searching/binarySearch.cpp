59e80fea0950a7567582884502487b49359a6a3d
ccx6mse9 <heissanjay@gmail.com>
2022-01-28 19:52:56 +0530

    Binary Search

---
+++ Algorithms/Searching/binarySearch.cpp
@@ -0,0 +1,9 @@
+// implementation of binary search
+
+#include <iostream>
+using namespace std;
+
+// sorted array
+int arr[10] = {2, 13, 27, 45, 67, 77, 81, 89, 92, 98};
+int arrlen = sizeof(arr) / sizeof(arr[0]);
+int binarySearch(int arr[], int target, int arrlen);
