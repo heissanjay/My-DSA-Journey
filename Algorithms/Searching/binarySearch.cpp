f12fb1bc870109591a2669a2e6c849c0ed5303a2
ccx6mse9 <heissanjay@gmail.com>
2022-01-28 19:53:54 +0530

    Binary Search

--- Algorithms/Searching/binarySearch.cpp
+++ Algorithms/Searching/binarySearch.cpp
@@ -6,4 +6,10 @@ using namespace std;
 // sorted array
 int arr[10] = {2, 13, 27, 45, 67, 77, 81, 89, 92, 98};
 int arrlen = sizeof(arr) / sizeof(arr[0]);
-int binarySearch(int arr[], int target, int arrlen);
+int binarySearch(int arr[], int target, int arrlen); // function prototype
+
+int main(int argc, char const *argv[])
+{
+    cout << binarySearch(arr, 45, arrlen) << "\n";
+    return 0;
+}
