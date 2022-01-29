2562a1758bc0f1eb2037e067170a4cabdb768f6e
ccx6mse9 <heissanjay@gmail.com>
2022-01-29 12:05:18 +0530

    Binary Search tree

--- Trees/binSearchTree.cpp
+++ Trees/binSearchTree.cpp
@@ -10,3 +10,25 @@ struct Node
     struct Node *left;
     struct Node *right;
 };
+
+struct Node *createNode(int val)
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->item = val;
+    newNode->left = nullptr;
+    newNode->right = nullptr;
+
+    return newNode;
+}
+struct Node *insertNode(struct Node *node, int val)
+{
+    if (node == nullptr)
+        return createNode(val);
+
+    else if (val < node->item)
+        node->left = insertNode(node->left, val);
+    else
+        node->right = insertNode(node->right, val);
+
+    return node;
+}
