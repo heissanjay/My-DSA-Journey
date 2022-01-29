6288ea64cdd429dc7a7ef4b6e4be2e94723bcedb
ccx6mse9 <heissanjay@gmail.com>
2022-01-29 12:04:25 +0530

    Binary Search tree

---
+++ Trees/binSearchTree.cpp
@@ -0,0 +1,12 @@
+// implementation binary Search trees
+#include <iostream>
+#include <queue>
+using namespace std;
+
+// structure of node
+struct Node
+{
+    int item;
+    struct Node *left;
+    struct Node *right;
+};
