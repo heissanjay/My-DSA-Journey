68443519774cca92296bd0af5607651cd0ef2431
ccx6mse9 <heissanjay@gmail.com>
2022-02-03 14:23:08 +0530

    Avl tree

--- Trees/avl_tree.cpp
+++ Trees/avl_tree.cpp
@@ -72,3 +72,50 @@ struct Node *right_rotate(struct Node *y)
 
     return x; // x becomes root
 }
+
+// func to insert a new node
+struct Node *insertNode(struct Node *node, int val)
+{
+    // find the correct position to insert the using recursion
+    if (node == nullptr) // base case
+        return createNode(val);
+
+    if (val < node->item)
+        node->left = insertNode(node->left, val);
+    else if (val > node->item)
+        node->right = insertNode(node->right, val);
+    return node;
+
+    // updating balance factor and balance the tree
+    node->height = max(getHeight(node->left), getHeight(node->right)) + 1;
+
+    // get the balance factor of the node
+    int bal_fac = getBalanceFactor(node);
+    if (bal_fac > 1)
+    {
+        if (val < node->left->item)
+        {
+            return right_rotate(node);
+        }
+        else if (val > node->left->item)
+        {
+            node->left = left_rotate(node);
+            return right_rotate(node);
+        }
+    }
+    if (bal_fac < -1)
+    {
+        if (val > node->right->item)
+        {
+            return left_rotate(node);
+        }
+        else if (val < node->right->item)
+        {
+            node->right = right_rotate(node->right);
+            return left_rotate(node);
+        }
+    }
+    return node;
+}
+
+// to be continued soon
