d4ae9a4cf938d60b94448bed1bab8fc9c70e56d4
ccx6mse9 <heissanjay@gmail.com>
2022-02-06 11:09:25 +0530

    Selection Sort

--- Algorithms/Sorting/selectionSort.cpp
+++ Algorithms/Sorting/selectionSort.cpp
@@ -5,6 +5,37 @@ using namespace std;
 
 int arr[] = {6, 5, 12, 10, 9, 1};
 int n = sizeof(arr) / sizeof(arr[0]); // n = 6
+// func to swap two variables
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[min] > arr[j])
+            {
+                min = j;
+            }
+        }
+        swap(&arr[min], &arr[i]);
+    }
+}
+void display(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
 
 int main(int argc, char const *argv[])
 {
