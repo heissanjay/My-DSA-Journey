1f6a07bbd7e83ffee1e1f90c8f5f293d49af14dd
ccx6mse9 <heissanjay@gmail.com>
2022-02-14 12:34:43 +0530

    added problem set

--- Problem-set/Binary-Search/peak_index.cpp
+++ Problem-set/Binary-Search/peak_index.cpp
@@ -1,1 +1,25 @@
 // https://leetcode.com/problems/peak-index-in-a-mountain-array/
+
+class Solution
+{
+public:
+    int peakIndexInMountainArray(vector<int> &arr)
+    {
+        int s = 0;
+        int e = arr.size() - 1;
+
+        while (s < e)
+        {
+            int m = s + (e - s) / 2;
+            if (arr[m] > arr[m + 1])
+            {
+                e = m;
+            }
+            else
+            {
+                s = m + 1;
+            }
+        }
+        return s;
+    }
+};
