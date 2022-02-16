996a816e2c66b6d73b17a48673592346a7e730aa
ccx6mse9 <heissanjay@gmail.com>
2022-02-16 18:13:08 +0530

    added problem set

--- Problem-set/Binary-Search/infinite_search.cpp
+++ Problem-set/Binary-Search/infinite_search.cpp
@@ -19,3 +19,24 @@ int solution(vector<int> arr, int target)
     }
     return binarySearch(arr, target, s, e);
 }
+int binarySearch(vector<int> arr, int target, int start, int end)
+{
+    while (start <= end)
+    {
+        int mid = start + (end - start) / 2;
+
+        if (target < arr[mid])
+        {
+            end = mid - 1;
+        }
+        else if (target == arr[mid])
+        {
+            return mid;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+    }
+    return -1;
+}
