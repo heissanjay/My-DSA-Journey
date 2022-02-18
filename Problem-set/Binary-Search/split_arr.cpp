ab0d7dd9538662ee3443100aacc3cb95131c78fb
ccx6mse9 <heissanjay@gmail.com>
2022-02-18 18:04:13 +0530

    added problem set

--- Problem-set/Binary-Search/split_arr.cpp
+++ Problem-set/Binary-Search/split_arr.cpp
@@ -1,1 +1,33 @@
 // https://leetcode.com/problems/split-array-largest-sum/
+
+class Solution
+{
+public:
+    int splitArray(vector<int> &nums, int m)
+    {
+        int start = *max_element(nums.begin(), nums.end());
+        int end = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            // start = max(start, nums[i]);
+            end += nums[i];
+        }
+        while (start < end)
+        {
+
+            int mid = start + (end - start) / 2;
+            int sum = 0;
+            int pieces = 1;
+            for (int num : nums)
+            {
+                if (sum + num > mid)
+                {
+                    sum = num;
+                    pieces++;
+                }
+                else
+                {
+                    sum += num;
+                }
+            }
