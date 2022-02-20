4794856982743da48f5bf770c2749ac0d2b0e754
ccx6mse9 <heissanjay@gmail.com>
2022-02-20 18:10:22 +0530

    added problem set

--- Problem-set/Linear-Search/max_wealth.cpp
+++ Problem-set/Linear-Search/max_wealth.cpp
@@ -1,1 +1,19 @@
 // https://leetcode.com/problems/richest-customer-wealth/
+
+class Solution
+{
+public:
+    int maximumWealth(vector<vector<int>> &accounts)
+    {
+        int ans = 0;
+        for (int i = 0; i < accounts.size(); i++)
+        {
+            int sum = 0;
+            for (int j = 0; j < accounts[i].size(); j++)
+            {
+                sum += accounts[i][j];
+            }
+        }
+        return ans;
+    }
+};
