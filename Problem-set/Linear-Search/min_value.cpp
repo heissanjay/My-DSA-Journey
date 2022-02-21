f4db21b8cb507fb6dda2a1ec515f9a38db8791f2
ccx6mse9 <heissanjay@gmail.com>
2022-02-21 17:53:27 +0530

    added problem set

---
+++ Problem-set/Linear-Search/min_value.cpp
@@ -0,0 +1,26 @@
+// find the minimum value in an array
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> arr = {23, 56, 78, 34, 15, 89};
+int findMin(vector<int> &nums)
+{
+    int ans = nums[0];
+
+    for (int i = 1; i < nums.size(); i++)
+    {
+        if (nums[i] < ans)
+        {
+            ans = nums[i];
+        }
+    }
+    return ans;
+}
+int main(int argc, char const *argv[])
+{
+    cout << findMin(arr) << "\n";
+
+    return 0;
+}
