9a829eee6beda4d9cf9c4e61de3f3b2b3961a3ab
ccx6mse9 <heissanjay@gmail.com>
2022-02-23 18:28:39 +0530

    added problem set

--- Problem-set/cyclic-sort/missing_num.cpp
+++ Problem-set/cyclic-sort/missing_num.cpp
@@ -28,3 +28,10 @@ public:
 
         return nums.size();
     }
+    void swap(vector<int> &arr, int a, int b)
+    {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+};
