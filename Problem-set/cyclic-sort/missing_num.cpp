679acce49e8c9886d592761c126fe01f0d14a98a
ccx6mse9 <heissanjay@gmail.com>
2022-02-23 18:28:03 +0530

    added problem set

--- Problem-set/cyclic-sort/missing_num.cpp
+++ Problem-set/cyclic-sort/missing_num.cpp
@@ -1,1 +1,30 @@
 // https://leetcode.com/problems/missing-number/
+
+class Solution
+{
+public:
+    int missingNumber(vector<int> &nums)
+    {
+        int i = 0;
+        while (i < nums.size())
+        {
+            int crt = nums[i];
+            if (nums[i] < nums.size() && nums[i] != nums[crt])
+            {
+                swap(nums, i, crt);
+            }
+            else
+            {
+                i++;
+            }
+        }
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] != i)
+            {
+                return i;
+            }
+        }
+
+        return nums.size();
+    }
