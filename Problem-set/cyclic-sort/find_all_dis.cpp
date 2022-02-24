dc0791e5441c225740cbb720b6dca65f2b6c882a
ccx6mse9 <heissanjay@gmail.com>
2022-02-24 18:12:33 +0530

    added problem set

--- Problem-set/cyclic-sort/find_all_dis.cpp
+++ Problem-set/cyclic-sort/find_all_dis.cpp
@@ -1,1 +1,31 @@
 // https://leetcode.com/problems/find-all-numbers-disappeared-in-an-array/
+
+class Solution
+{
+public:
+    vector<int> findDisappearedNumbers(vector<int> &nums)
+    {
+        int i = 0;
+        while (i < nums.size())
+        {
+            int crt = nums[i] - 1;
+            if (nums[i] != nums[crt])
+            {
+                swap(nums, i, crt);
+            }
+            else
+            {
+                i++;
+            }
+        }
+        vector<int> ans;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] != i + 1)
+            {
+                ans.push_back(i + 1);
+            }
+        }
+        return ans;
+    }
+};
