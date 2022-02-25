8e4d9cb8989dae1a937b0256ece88aa785ce9f58
ccx6mse9 <heissanjay@gmail.com>
2022-02-25 18:30:38 +0530

    added problem set

--- Problem-set/Cyclic-Sort/find_duplicates.cpp
+++ Problem-set/Cyclic-Sort/find_duplicates.cpp
@@ -1,1 +1,30 @@
 //  https://leetcode.com/problems/find-the-duplicate-number/
+
+class Solution
+{
+public:
+    int findDuplicate(vector<int> &nums)
+    {
+        int i = 0;
+        while (i < nums.size())
+        {
+            if (nums[i] != i + 1)
+            {
+                int crt = nums[i] - 1;
+                if (nums[i] != nums[crt])
+                {
+                    swap(nums, i, crt);
+                }
+                else
+                {
+                    return nums[i];
+                }
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return -1;
+    }
+};
