315dd62c6649aa36c5547f7dbe8993c395facc0a
ccx6mse9 <heissanjay@gmail.com>
2022-02-26 17:20:57 +0530

    added problem set

--- Problem-set/Cyclic-Sort/find_all_duplicates.cpp
+++ Problem-set/Cyclic-Sort/find_all_duplicates.cpp
@@ -1,1 +1,31 @@
 // https://leetcode.com/problems/find-all-duplicates-in-an-array/
+
+class Solution
+{
+public:
+    vector<int> findDuplicates(vector<int> &nums)
+    {
+        int i = 0;
+        while (i < nums.size())
+        {
+            int crt = nums[i] - 1;
+            if (nums[i] != nums[crt])
+            {
+                swap(nums, i, crt);
+            }
+            else
+            {
+                i++;
+            }
+        }
+        vector<int> result;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] != i + 1)
+            {
+                result.push_back(nums[i]);
+            }
+        }
+        return result;
+    }
+};
