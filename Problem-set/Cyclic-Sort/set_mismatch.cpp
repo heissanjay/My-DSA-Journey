f1bbf2e8803444c03c82db1f2a4c4db6fd07fd70
ccx6mse9 <heissanjay@gmail.com>
2022-02-27 14:54:59 +0530

    added problem set

--- Problem-set/Cyclic-Sort/set_mismatch.cpp
+++ Problem-set/Cyclic-Sort/set_mismatch.cpp
@@ -1,1 +1,21 @@
 //  https://leetcode.com/problems/set-mismatch/'
+
+class Solution
+{
+public:
+    vector<int> findErrorNums(vector<int> &nums)
+    {
+
+        int i = 0;
+        while (i < nums.size())
+        {
+            int crt = nums[i] - 1;
+            if (nums[i] != nums[crt])
+            {
+                swap(nums, i, crt);
+            }
+            else
+            {
+                i++;
+            }
+        }
