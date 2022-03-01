a16ffe35a7b4913a0aa2f6eee5e0ee317b515a09
ccx6mse9 <heissanjay@gmail.com>
2022-03-01 18:47:38 +0530

    added problem set

--- Problem-set/Cyclic-Sort/missing_positive.cpp
+++ Problem-set/Cyclic-Sort/missing_positive.cpp
@@ -1,1 +1,28 @@
 // https://leetcode.com/problems/first-missing-positive
+
+class Solution
+{
+public:
+    int firstMissingPositive(vector<int> &nums)
+    {
+        if (nums.size() == 0)
+            return 1;
+        int res = nums.size() + 1;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            while (nums[i] <= nums.size() && nums[i] > 0 && nums[i] != nums[nums[i] - 1])
+            {
+                swap(nums[i], nums[nums[i] - 1]);
+            }
+        }
+        for (int i = 0; i < nums.size(); ++i)
+        {
+            if (nums[i] != i + 1)
+            {
+                res = i + 1;
+                break;
+            }
+        }
+        return res;
+    }
+};
