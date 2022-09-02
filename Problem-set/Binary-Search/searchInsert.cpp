fc1f273420abebf17c2c903c8368451c7cf8f4ac
sanjay <heissanjay@gmail.com>
2022-09-02 12:36:36 +0530

    added a new problem

--- Problem-set/Binary-Search/searchInsert.cpp
+++ Problem-set/Binary-Search/searchInsert.cpp
@@ -1,1 +1,21 @@
 // https://leetcode.com/problems/search-insert-position/
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int s = 0;
+        int e = nums.size() - 1;
+        
+        while( s <= e){
+            int m = (s + e) / 2;
+            if(target < nums[m]){
+                e = m - 1;
+            }else if(target > nums[m]){
+                s = m + 1;
+            }else{
+                return m;
+            }
+        }
+        return s;
+    }
+};
