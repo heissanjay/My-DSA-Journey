6471ebb54ff7b3bf6475130776655886843f72fb
sanjay <heissanjay@gmail.com>
2022-12-10 18:01:01 +0530

    ProblemSet: Binary-Tree

---
+++ Problem-set/Binary-tree/max_product.cpp
@@ -0,0 +1,44 @@
+// problem-link
+// https://leetcode.com/problems/maximum-product-of-splitted-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    long long totalSum = 0;
+    long long productMax = 0;
+    int mod  = 1e9 + 7;
+    // traverse the tree and find the total sum
+    void find_the_totalsum(TreeNode* root, long long &sum){
+        if(root == NULL){
+            return;
+        }
+        totalSum+=root->val;
+        find_the_totalsum(root->left,totalSum);
+        find_the_totalsum(root->right,totalSum);
+    }
+    long long calc_max_prod(TreeNode* root){
+        if(root == NULL) return 0;
+
+        long long left = calc_max_prod(root->left);
+        long long right = calc_max_prod(root->right);
+
+        long long subtree_sum = left + right + root->val;
+        productMax = max(productMax, (subtree_sum) * (totalSum - subtree_sum));
+        return subtree_sum;
+    }
+    int maxProduct(TreeNode* root) {
+        find_the_totalsum(root,totalSum);
+        calc_max_prod(root);
+        return (int)(productMax % mod);
+    }
+// };
