42b42ed512f28a7932b2c9f5710d716d4e0abac9
sanjay <heissanjay@gmail.com>
2022-12-11 19:09:35 +0530

    ProblemSet: Binary-Tree

---
+++ Problem-set/Binary-tree/max_path_sum.cpp
@@ -0,0 +1,20 @@
+
+
+
+class Solution {
+private:
+    int maxSum = INT_MIN;
+public:
+    int traverse_postorder(TreeNode* root){
+        if(root == NULL) return 0;
+        int left = max(traverse_postorder(root->left),0);
+        int right = max(traverse_postorder(root->right),0);
+
+        maxSum = max(maxSum, left+right+root->val);
+        return max(left,right) + root->val;
+    }
+    int maxPathSum(TreeNode* root) {
+       traverse_postorder(root);
+       return maxSum;
+    }
+};
