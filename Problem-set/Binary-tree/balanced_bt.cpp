5c48cd921b28d32fa30ee724680e09f128330e26
sanjay <heissanjay@gmail.com>
2022-12-13 19:07:19 +0530

    ProblemSet: Binary-Tree

--- Problem-set/Binary-tree/balanced_bt.cpp
+++ Problem-set/Binary-tree/balanced_bt.cpp
@@ -21,3 +21,21 @@ public:
     }
 };
 
+//  Approach - 2
+class Solution {
+public:
+    int dfs(TreeNode* root){
+        if(root == nullptr) return 0;
+
+        int lh = dfs(root->left);
+        if(lh == -1) return -1;
+        int rh = dfs(root->right);
+        if(rh == -1) return -1;
+        // if at any point of the
+        if(abs(lh-rh) > 1) return -1;
+        return max(lh,rh) + 1;
+    }
+    bool isBalanced(TreeNode* root) {
+        return dfs(root) != -1;
+    }
+};
